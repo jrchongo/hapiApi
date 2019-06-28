9a8419f5f6ac3df03a17d5ad110ddfbc029afb87
Joe <jrchongo@gmail.com>
2019-06-27 19:58:33 -0500

    Configures mocha test runner.

---
+++ test/unit/get-routes.spec.js
@@ -0,0 +1,17 @@
+import {expect} from 'chai';
+
+describe('Get Routes', () => {
+    let x = 0;
+
+    beforeEach(() => {
+        x++;
+    });
+
+    it('should do something', () => {
+        expect(1).to.equal(x);
+    });
+
+    it('should do something', () => {
+        expect(2).to.equal(x);
+    });
+});
